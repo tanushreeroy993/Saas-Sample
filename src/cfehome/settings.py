2b8515d060cff27551d159ff661b5f970cc404ff
Tanushree Roy <tanushreeroy993@gmail.com>
2024-08-28 11:12:23 +0530

    Hello World

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -24,7 +24,14 @@ SECRET_KEY = 'django-insecure-46!6i%3gut@f9fo5l*2&m)(k^x**5_gjwq8+^i5t#h8)ii9tsc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+if  DEBUG :
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -7,7 +7,11 @@ from visits.models import PageVisit
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+    
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path = request.path)
     my_title = "My Page"
@@ -15,18 +19,10 @@ def home_page_view(request, *args, **kwargs):
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
         "total_visit_count": qs.count(),
-
     }
+
     path = request.path
     print("path",path)
     html_temp = "home.html"
     PageVisit.objects.create(path = request.path)
     return render(request, html_temp, my_context)
-    
-
-def old_home_page_view(request, *args, **kwargs):
-    print(this_dir)
-    html_ = ""
-    html_file_path = this_dir / "home.html"
-    html_ = html_file_path.read_text()
-    return HttpResponse(html_)
